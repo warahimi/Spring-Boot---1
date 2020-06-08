bb404279a8f91ab9a1969b057d39b346adcf82cc
Wahid1 <Wahid1@DESKTOP-DE4OEA8>
2020-06-07 22:04:13 -0700

    Spring Data REST : No controller methods and class

---
+++ SpringBootWebApp-Telusko2-SpringDataRest/src/main/java/com/telsuko/SpringBootWebAppTelusko2SpringDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.telsuko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebAppTelusko2SpringDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppTelusko2SpringDataRestApplication.class, args);
+	}
+
+}
